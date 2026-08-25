e803147e422ca6ee8af81c5fa92593c6425085fd
tt-a1i <tt-a1i@users.noreply.github.com>
2026-08-25 21:14:26 +0800

    feat: add SkillRoster 1.8.28 formula
    Closes #1.
    
    Signed-off-by: tt-a1i <53142663+tt-a1i@users.noreply.github.com>

---
+++ Formula/skillroster.rb
@@ -0,0 +1,25 @@
+class Skillroster < Formula
+  desc "Local skill governance for AI agents"
+  homepage "https://github.com/tt-a1i/skillroster"
+  url "https://github.com/tt-a1i/skillroster/archive/refs/tags/v1.8.28.tar.gz"
+  sha256 "23dddf240c729eb73f8117ed1b27c17f5f2fbdb67e975e2903410b6cd618879c"
+  license "Apache-2.0"
+  head "https://github.com/tt-a1i/skillroster.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/skillroster --version")
+
+    output = shell_output(
+      "#{bin}/skillroster scan --summary --json " \
+      "--home #{testpath}/home --state-dir #{testpath}/state",
+    )
+    assert_match '"ok":true', output
+    assert_match '"files_changed":false', output
+  end
+end
