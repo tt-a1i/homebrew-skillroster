7107873827cd3ea8e1ec2a2dd93172b478d60371
tt-a1i <53142663+tt-a1i@users.noreply.github.com>
2026-09-03 13:31:04 +0000

    skillroster: add 1.8.45 bottle.

--- Formula/skillroster.rb
+++ Formula/skillroster.rb
@@ -6,6 +6,12 @@ class Skillroster < Formula
   license "Apache-2.0"
   head "https://github.com/tt-a1i/skillroster.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/tt-a1i/homebrew-skillroster/releases/download/skillroster-1.8.45"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "d9a1a5091c6c81d208743a50d99b4daf4481849967254476703ca11c664155fe"
+    sha256 cellar: :any,                 x86_64_linux: "f85bf30512892d5362f0f73e5cdc355d85dba91abcc67887846531d43df9e2c2"
+  end
+
   depends_on "rust" => :build
 
   def install
